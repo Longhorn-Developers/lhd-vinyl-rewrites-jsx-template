aa6e8dc48099acb1d43478c0afd01c418342f258
Samuel Gunter <samathingamajig@gmail.com>
2025-03-29 16:43:59 -0500

    step 7: extract NavBar icon

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -5,26 +5,7 @@ export function NavBar() {
         <nav className='navbar'>
             <h1>
                 <Link to='/'>
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        width='1em'
-                        height='1em'
-                        viewBox='0 0 24 24'
-                        className='vinyl-icon'
-                    >
-                        <g
-                            fill='none'
-                            stroke='currentColor'
-                            stroke-linecap='round'
-                            stroke-linejoin='round'
-                            stroke-width='2'
-                        >
-                            <path d='M16 3.937A9 9 0 1 0 21 12'></path>
-                            <path d='M11 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0m8-8a1 1 0 1 0 2 0a1 1 0 1 0-2 0'></path>
-                            <path d='m20 4l-3.5 10l-2.5 2'></path>
-                        </g>
-                    </svg>
-                    Vinyl Rewrites
+                    <NavBarIcon /> Vinyl Rewrites
                 </Link>
             </h1>
             <ul>
@@ -36,5 +17,17 @@ export function NavBar() {
                 </li>
             </ul>
         </nav>
+    );
+}
+
+function NavBarIcon() {
+    return (
+        <svg xmlns='http://www.w3.org/2000/svg' width='1em' height='1em' viewBox='0 0 24 24' className='vinyl-icon'>
+            <g fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2'>
+                <path d='M16 3.937A9 9 0 1 0 21 12'></path>
+                <path d='M11 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0m8-8a1 1 0 1 0 2 0a1 1 0 1 0-2 0'></path>
+                <path d='m20 4l-3.5 10l-2.5 2'></path>
+            </g>
+        </svg>
     );
 }
