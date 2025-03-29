d6bcec31bac03b66c21a6d1f0ee223748282cc19
Samuel Gunter <samathingamajig@gmail.com>
2025-03-29 16:05:51 -0500

    step 5: basic navbar

--- app/routes/__root.jsx
+++ app/routes/__root.jsx
@@ -62,6 +62,40 @@ function RootDocument({ children }) {
                 <HeadContent />
             </head>
             <body>
+                <nav className='navbar'>
+                    <h1>
+                        <a href='.'>
+                            <svg
+                                xmlns='http://www.w3.org/2000/svg'
+                                width='1em'
+                                height='1em'
+                                viewBox='0 0 24 24'
+                                className='vinyl-icon'
+                            >
+                                <g
+                                    fill='none'
+                                    stroke='currentColor'
+                                    stroke-linecap='round'
+                                    stroke-linejoin='round'
+                                    stroke-width='2'
+                                >
+                                    <path d='M16 3.937A9 9 0 1 0 21 12'></path>
+                                    <path d='M11 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0m8-8a1 1 0 1 0 2 0a1 1 0 1 0-2 0'></path>
+                                    <path d='m20 4l-3.5 10l-2.5 2'></path>
+                                </g>
+                            </svg>
+                            Vinyl Rewrites
+                        </a>
+                    </h1>
+                    <ul>
+                        <li>
+                            <a href='.'>Home</a>
+                        </li>
+                        <li>
+                            <a href='./about/'>About</a>
+                        </li>
+                    </ul>
+                </nav>
                 <div className='app'>{children}</div>
                 <Scripts />
             </body>
