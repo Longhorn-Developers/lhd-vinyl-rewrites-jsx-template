648c48ff22f7460e95ff15ab4880c18200d1da51
Samuel Gunter <samathingamajig@gmail.com>
2025-03-29 17:14:58 -0500

    step 9: full albumId page

--- app/routes/albums/$albumId.jsx
+++ app/routes/albums/$albumId.jsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, Link } from '@tanstack/react-router';
 import { albumsById } from '../../albums';
 
 export const Route = createFileRoute('/albums/$albumId')({
@@ -11,8 +11,37 @@ function RouteComponent() {
     const album = albumsById.get(albumId);
 
     if (!album) {
-        return <div>Album not found</div>;
+        return (
+            <div className='album-not-found'>
+                <h2>Album not found</h2>
+                <Link to='/' className='back-link'>
+                    <span>←</span> Back to Albums
+                </Link>
+            </div>
+        );
     }
 
-    return <pre>{JSON.stringify(album, null, 4)}</pre>;
+    return (
+        <div className='album-detail-container'>
+            <div className='album-detail-navigation'>
+                <Link to='/' className='back-link'>
+                    <span>←</span> Back to Albums
+                </Link>
+            </div>
+            <div className='album-detail-content'>
+                <a className='album-cover-container' href={album.coverUrl}>
+                    <img src={album.coverUrl} alt={album.title} className='album-detail-cover' />
+                </a>
+                <div className='album-info'>
+                    <h1>{album.title}</h1>
+                    <h2 className='album-artist'>{album.artist}</h2>
+                    <div className='album-metadata'>
+                        <div className='metadata-item'>
+                            <span>Released: {album.year}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
